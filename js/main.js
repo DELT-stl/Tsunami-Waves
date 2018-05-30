8250492fc56abf6fab7cb06fffe5714e0229ae76
josh581 <josh@deltstl.com>
2018-05-30 12:08:50 -0500

    page transition class

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    $('.slantedin h2').on('click', function() {
+    $('h2').html('hello');
+        $('.slanted img').addClass('page-transition');
+    });
+
+});
