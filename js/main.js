8b1593c48968db1d6334f4578dd697f60c4b37da
josh581 <josh@deltstl.com>
2018-06-01 13:09:38 -0500

    fixed issue where if image was above scrollTop, it would malfunction

--- js/main.js
+++ js/main.js
@@ -3,29 +3,31 @@ jQuery(document).ready(function ($) {
         var obj = $(this);
         var scroll = $(window).scrollTop(),
             elementOffset = $('.anchor').offset().top,
-            distance = (elementOffset - scroll),
-            w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-            h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-            l = $(this).offset().left,
-            right = w - l,
-            scaleX = w / $(this).width(),
-            scaleY = h / $(this).height();
+            distance = (elementOffset - scroll);
+        //            w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        //            h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+        //            l = $(this).offset().left;
         //        $('.original-page').html('');
         //        $('html').scrollTop(0);
         $(this).parent().siblings().animate({
             'opacity': '0'
         });
-        $('.anchor').css({});
         $('.anchor .wrapper').css({
             "transform": "skew(0deg)",
             "-webkit-transform": "skew(0deg)",
             "-ms-transform": "skew(0deg)"
         });
+        if ($(this).offset().top < scroll) {
+            d = distance * -1;
+            $('.anchor').animate({
+                'margin-top': d
+            });
+        }
         $(this).find('.slantedin').animate({
             'opacity': '0'
         });
         $(this).css({
-            'position': 'absolute',
+            'position': 'fixed',
         });
         $(this).parent().css({
             'z-index': '2'
@@ -38,80 +40,38 @@ jQuery(document).ready(function ($) {
             'width': '100vw'
 
         });
-        if ($(this).offset().top < scroll) {
-            $('.anchor .wrapper').css({
-                'position': 'absolute'
-            });
-            $('.anchor').animate({
-                'top': '-' + distance
-            });
-        }
         $('#content-div *, .original-page *').animate({
             'opacity': '0'
         });
         setTimeout(function () {
-            obj.parent().siblings().html('').css({
-                'display':'none'
-            });
-            $('.anchor .wrapper').css({
-                'position': 'relative'
+            $('.anchor').css({
+                'margin-top':'0'
             });
             obj.css({
-                'position':'relative'
+                'position': 'relative',
+                'top': '0'
+            }).parent().css({
+                'padding-left': '0',
+                'padding-right': '0'
+            }).siblings().html('').css({
+                'display': 'none'
             });
-            obj.parent().css({
-                'padding-left':'0',
-                'padding-right':'0'
-            });
-            $('.slanted').css({
-                'top': 0
-            });
-            // Do something after 1 second
-            $('.original-page').html('');
-            //            var href = '/mission.html'
+            $('.original-content').html('');
             var href = obj.data('url');
-
-
             console.log(href);
-            //        $('#content-div').html('');
-            $('#content-div').load(href + ' #content-div');
             $('#content-div').css({
                 'opacity': '0'
-            });
-            $('#content-div').animate({
+            }).load(href + ' #content-div').animate({
                 'opacity': '1'
             });
             $('html').scrollTop(0);
-            // loads content into a div with the ID content_div
-
             // HISTORY.PUSHSTATE
             history.pushState('', 'New URL: ' + href, href);
             window.onpopstate = function (event) {
                 location.reload();
             };
-            //            response.headers['Vary'] = 'Accept';
-            //            window.onpopstate = function (event) {
-            //                alert("location: " + document.location + ", state: " + JSON.stringify(event.state));
-            //                            location.reload();
-            //                        response.headers['Vary'] = 'Accept';
-            //            };
-
-            //            $(window).bind('popstate', function () {
-
-            //            window.onpopstate = function (event) {
-            //                window.location.href = window.location.href;
-            //                location.reload();
-            //            };
             event.preventDefault();
-
-
         }, 1000);
 
     });
-
-    function save($obj) {
-        var l = $obj;
-        return $obj;
-    }
-
 });
