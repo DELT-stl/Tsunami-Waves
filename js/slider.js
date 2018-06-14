bf7f2472ec57152fd4ed278972f7b22f560705c2
stldelt <josh@deltstl.com>
2018-06-14 08:25:00 -0500

    Home and Mission

--- js/slider.js
+++ js/slider.js
@@ -73,7 +73,7 @@ var imagesToLoad = null;
 
 var
 slideshow,
-	slideshowDuration = 4000,
+	slideshowDuration = 6000,
 	loaderAnim = true;
 
 /**
@@ -320,33 +320,3 @@ sliderPages();
 homeSlider();
 
 
-$(document).ready(function() {
-	if ($("js-parallax-window").length) {
-		parallax();
-	}
-});
-
-$(window).scroll(function(e) {
-	if ($("#js-parallax-window").length) {
-		parallax();
-	}
-});
-
-function parallax(){
-	if( $("#js-parallax-window").length > 0 ) {
-		var plxBackground = $("#js-parallax-background");
-		var plxWindow = $("#js-parallax-window");
-
-		var plxWindowTopToPageTop = $(plxWindow).offset().top;
-		var windowTopToPageTop = $(window).scrollTop();
-		var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
-
-		var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-		var windowInnerHeight = window.innerHeight;
-		var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-		var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-		var plxSpeed = 0.35;
-
-		plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
-	}
-}
