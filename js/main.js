66adba3ac6eab026c6ea9af706152ab469f9533e
josh581 <josh@deltstl.com>
2018-06-18 12:26:16 -0500

    put js in main.js at callback of fade transition

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($) {
   console.log('document ready');
   if (Modernizr.history) {
-        slideShowInit();
-        initParalax();
+    slideShowInit();
+    initParalax();
     $(window).bind("popstate", function() {
       link = location.pathname.replace(/^.*[\\/]/, ""); // get filename only
       //            alert(link);
